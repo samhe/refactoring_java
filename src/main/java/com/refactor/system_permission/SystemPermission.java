3dca3d20a604d14cf113cd9dbaff7f7aaa992848
Sam He <sam.he@cargosmart.com>
2017-12-15 17:48:49 +0800

    5. clean the useless code in system permission.

--- src/main/java/com/refactor/system_permission/SystemPermission.java
+++ src/main/java/com/refactor/system_permission/SystemPermission.java
@@ -1,32 +1,30 @@
 package com.refactor.system_permission;
 
 public class SystemPermission {
+    public final static String REQUESTED = "REQUESTED";
+    public final static String CLAIMED = "CLAIMED";
+    public final static String GRANTED = "GRANTED";
+    public final static String DENIED = "DENIED";
+    public final static String UNIX_REQUESTED = "UNIX_REQUESTED";
+    public final static String UNIX_CLAIMED = "UNIX_CLAIMED";
+
     private SystemProfile profile;
 
     private SystemAdmin admin;
 
-
-    private String state;
-
     private PermissionState permissionState;
 
-    public final static String REQUESTED = "REQUESTED";
-
-    public final static String CLAIMED = "CLAIMED";
-
-    public final static String GRANTED = "GRANTED";
-    public final static String DENIED = "DENIED";
-    public static final String UNIX_REQUESTED = "UNIX_REQUESTED";
-    public static final String UNIX_CLAIMED = "UNIX_CLAIMED";
     public SystemPermission(SystemProfile profile) {
         this.profile = profile;
-        state = REQUESTED;
-        notifyAdminOfPermissionRequest();
         permissionState = new RequestedSate(this);
     }
-    public void notifyAdminOfPermissionRequest() {
 
-    }
+    public void notifyAdminOfPermissionRequest() { }
+
+    public void notifyUserOfPermissionRequestResult() { }
+
+    public void notifyUnixAdminsOfPermissionRequest() { }
+
     public void setPermissionState(PermissionState permissionState) {
         this.permissionState = permissionState;
     }
@@ -35,31 +33,15 @@ public class SystemPermission {
         return profile;
     }
 
-    public SystemAdmin getAdmin() {
-        return admin;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
     public void setAdmin(SystemAdmin admin) {
         this.admin = admin;
     }
 
     public boolean isGranted() {
-        return GRANTED.equals(this.state);
+        return GRANTED.equals(state());
     }
 
     public void claimedBy(SystemAdmin admin) {
-        if (state.equals(REQUESTED)) {
-            setAdmin(admin);
-            setState(CLAIMED);
-        }
-        else if (state.equals(UNIX_REQUESTED)) {
-            setAdmin(admin);
-            setState(UNIX_CLAIMED);
-        }
         permissionState.claimBy(admin);
     }
 
@@ -68,44 +50,15 @@ public class SystemPermission {
     }
 
     public void deniedBy(SystemAdmin admin) {
-        if(isClaimedBy(admin)) {
-            if(state.equals(CLAIMED) || state.equals(UNIX_CLAIMED)) {
-                setState(DENIED);
-                notifyUserOfPermissionRequestResult();
-            }
-        }
         permissionState.denyBy(admin);
     }
 
-    public void notifyUserOfPermissionRequestResult() {
-
-    }
-
     public void grantedBy(SystemAdmin admin) {
-        if(isClaimedBy(admin)) {
-            if(state.equals(CLAIMED) && !profile.isUnixPermissionRequired()) {
-                setState(GRANTED);
-                notifyUserOfPermissionRequestResult();
-            } else if (state.equals(CLAIMED) && profile.isUnixPermissionRequired()) {
-                setState(UNIX_REQUESTED);
-                profile.setUnixPermissionGranted(true);
-                notifyUnixAdminsOfPermissionRequest();
-            } if (state.equals(UNIX_CLAIMED) && profile.isUnixPermissionRequired()) {
-                if(profile.isUnixPermissionGranted()) {
-                    setState(GRANTED);
-                    notifyUserOfPermissionRequestResult();
-                }
-            }
-        }
         permissionState.grantBy(admin);
     }
 
-    public void notifyUnixAdminsOfPermissionRequest() {
-
-    }
-
     public String state() {
-        return this.state;
+        return permissionState.getState();
     }
 
 }
